808162e2881d3c2129c23bbfc9d5c060b27e9282
Daniel <damasio.dan7@gmail.com>
2022-05-20 09:07:29 -0300

    add to repostory files

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ session_start();
 
 use App\Core;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once('../vendor/autoload.php');
 
 $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();

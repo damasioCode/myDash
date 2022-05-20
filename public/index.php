ac4cacae378b88e9f99d38cba8d4699c99e2ef34
Daniel <damasio.dan7@gmail.com>
2022-05-20 09:35:47 -0300

    updated: heroku files

--- public/index.php
+++ public/index.php
@@ -3,9 +3,9 @@ session_start();
 
 use App\Core\Core;
 
-require_once('./../vendor/autoload.php');
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = \Dotenv\Dotenv::createImmutable('./../');
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 $core = new Core();

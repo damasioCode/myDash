e77b30bbb06fb75415198a2dd58e12f3cbda737a
Daniel <damasio.dan7@gmail.com>
2022-05-21 17:32:09 -0300

    update: heroku files

--- src/App/Core/Template.php
+++ src/App/Core/Template.php
@@ -6,7 +6,7 @@ class Template
 {
 
     public static function getUrl() {
-        return $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/myDash/src';
+        return $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] ;
     }
 
 }

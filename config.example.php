a35c2564d7361d7292c677284ed41973ec754e3a
Daniel <damasio.dan7@gmail.com>
2022-05-29 21:48:44 -0300

    added : config file

--- config.example.php
+++ config.example.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_DRIVER', 'mysql');
-define('DB_HOST', '');
-define('DB_NAME', '');
-define('DB_USER', '');
-define('DB_PASS', '');
+define('DB_HOST', 'us-cdbr-east-05.cleardb.net');
+define('DB_NAME', 'heroku_06acb0bfbd6a575');
+define('DB_USER', '61af6140');
+define('DB_PASS', 'b142912509004c');
 
-define('BASE_PATH', '/myDash');
+define('BASE_PATH', '/');

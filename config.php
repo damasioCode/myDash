0072de4b76a8e2d09eadf77f79041e94411d5319
Daniel <damasio.dan7@gmail.com>
2022-05-29 21:27:17 -0300

    Untracked files issue resolved to fix .gitignore

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DB_DRIVER', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'mydash');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-
-define('BASE_PATH', '/myDash');
